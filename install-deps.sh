6a9dfb3bc19c235f82d614de9d7854ce05c8a26d
Gennadii Zakharov <Gennadiy.Zakharov@gmail.com>
2026-05-03 13:07:31 +0100

    Remove podman-docker from install-deps.sh dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 sudo apt update
-sudo apt install -y podman podman-docker uidmap
+sudo apt install -y podman uidmap
 
 # sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER
 
